8d13a78729bb6be986bf5fe65b87a2c40c479078
dsokol3 <dsokol3@student.touro.edu>
2025-12-28 22:51:16 -0500

    homework

--- src/main/java/edu/touro/mcon264/sorting/MergeSort.java
+++ src/main/java/edu/touro/mcon264/sorting/MergeSort.java
@@ -5,7 +5,45 @@ import java.util.Comparator;
 public class MergeSort implements Sorter {
 
     @Override
+    @SuppressWarnings("unchecked")
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement merge sort
+        if (a.length < 2) {
+            return;
+        }
+        
+        // Split array in half
+        int mid = a.length / 2;
+        T[] left = (T[]) new Object[mid];
+        T[] right = (T[]) new Object[a.length - mid];
+        
+        // Copy elements
+        for (int i = 0; i < mid; i++) {
+            left[i] = a[i];
+        }
+        for (int i = mid; i < a.length; i++) {
+            right[i - mid] = a[i];
+        }
+        
+        // Recursively sort both halves
+        sort(left, comp);
+        sort(right, comp);
+        
+        // Merge them back together
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (comp.compare(left[i], right[j]) <= 0) {
+                a[k++] = left[i++];
+            } else {
+                a[k++] = right[j++];
+            }
+        }
+        
+        // Copy any remaining elements
+        while (i < left.length) {
+            a[k++] = left[i++];
+        }
+        while (j < right.length) {
+            a[k++] = right[j++];
+        }
     }
 }
